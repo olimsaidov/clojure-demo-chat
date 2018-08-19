d515cda297e52d309d8671dd18de433b29903c72
Olim Saidov <olim.mail@gmail.com>
2018-08-19 10:12:37 +0500

    Initial commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject demo "0.0.1"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [http-kit/http-kit "2.3.0"]
+                 [compojure "1.6.1"]
+                 [selmer "1.12.0"]])
